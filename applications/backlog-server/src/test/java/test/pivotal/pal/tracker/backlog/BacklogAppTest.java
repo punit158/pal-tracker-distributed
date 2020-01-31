0e518a6eb4242fc44f2e39e9a07737b02863b718
pu20139798 <punit.bangade@wipro.com>
2020-01-31 15:46:57 -0800

    unit testing

--- applications/backlog-server/src/test/java/test/pivotal/pal/tracker/backlog/BacklogAppTest.java
+++ applications/backlog-server/src/test/java/test/pivotal/pal/tracker/backlog/BacklogAppTest.java
@@ -12,7 +12,7 @@ public class BacklogAppTest {
     public void embedded() {
         App.main(new String[]{});
 
-        String response = new RestTemplate().getForObject("http://localhost:8181/stories?projectId=0", String.class);
+        String response = new RestTemplate().getForObject("http://localhost:8082/stories?projectId=0", String.class);
 
         assertThat(response).isEqualTo("[]");
     }
--- applications/timesheets-server/src/test/java/test/pivotal/pal/tracker/timesheets/TimesheetsAppTest.java
+++ applications/timesheets-server/src/test/java/test/pivotal/pal/tracker/timesheets/TimesheetsAppTest.java
@@ -12,7 +12,7 @@ public class TimesheetsAppTest {
     public void embedded() {
         App.main(new String[]{});
 
-        String response = new RestTemplate().getForObject("http://localhost:8181/time-entries?userId=0", String.class);
+        String response = new RestTemplate().getForObject("http://localhost:8084/time-entries?userId=0", String.class);
 
         assertThat(response).isEqualTo("[]");
     }
